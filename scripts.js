61327eb373a8b3c78b438aa17c649383b4c44995
Ugur <ugurugurov11@gmail.com>
2025-11-03 07:37:09 +0300

    built js logic

--- scripts.js
+++ scripts.js
@@ -1,20 +1,25 @@
-function getLabelHtml() {
-    // <div class="label-card">
-    //     <p class="dear">Dear </p>
-    //     <p></p>
-    //     <p>Best wishes, </p>
-    //     <p></p>
-    // </div>
+const text = 'Thank you for all your hard work throughout the year🙏🎁'
+const sender = 'Ugur'
+
+function getLabelHtml(text, sender, ...names) {
+    const labelHtml = names.map(staff => 
+        `<div class="label-card">
+            <p class="dear">Dear ${staff.name},</p>
+            <p>${text}</p>
+            <p>Best wishes,</p>
+            <p>${sender}</p>
+        </div>`
+    ).join('')
+    return labelHtml
 }
 
-const text = 'Thank you for all your hard work throughout the year🙏🎁'
-const sender = 'Tom'
-
-document.getElementById('labels-container').innerHTML = getLabelHtml (
+document.querySelector('.labels-container').innerHTML = getLabelHtml (
     text,
     sender,
     {name: 'Sally'},
     {name: 'Mike'},
     {name: 'Rob'},
-    {name: 'Harriet'}
+    {name: 'Harriet'},
+    {name: 'Brandon'},
+    {name: 'Stacy'}
 )
